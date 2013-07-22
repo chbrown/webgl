4f8800d0601ad0a6ea94ef4bbda351c071657ace
Christopher Brown <io@henrian.com>
2013-07-22 15:11:03 -0500

    WebGL sandbox, initial commit. Two shapes!

---
+++ README.md
@@ -0,0 +1,4 @@
+# Examples involving either maps, heatmaps, or large numbers of small things.
+
+* http://aleksandarrodic.com/p/daily-routines/
+* [gl-matrix documentation](http://glmatrix.net/docs/2.2.0/)
---
+++ gl-helpers.js
@@ -0,0 +1,98 @@
+"use strict"; /*jslint indent: 2, debug: true, devel: true, browser: true */ /*globals $, Event, CustomEvent */
+
+function getShader(gl, selector) {
+  var el = document.querySelector(selector);
+  var type = el.getAttribute('type');
+  var src = el.innerText;
+
+  var shader = null;
+  if (type == "x-shader/x-fragment") {
+    shader = gl.createShader(gl.FRAGMENT_SHADER);
+  }
+  else if (type == "x-shader/x-vertex") {
+    shader = gl.createShader(gl.VERTEX_SHADER);
+  }
+  else {
+    throw new Error('Unsupported shader type: ' + type);
+  }
+
+  gl.shaderSource(shader, src);
+  gl.compileShader(shader);
+
+  // Ensure that it compiled successfully
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    throw new Error('Could not compile shader: ' + gl.getShaderInfoLog(shader));
+  }
+
+  return shader;
+}
+
+function getShaderProgram(gl, selectors) {
+  var program = gl.createProgram();
+
+  selectors.forEach(function(selector) {
+    var shader = getShader(gl, selector);
+    gl.attachShader(program, shader);
+  });
+
+  gl.linkProgram(program);
+
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    throw new Error('Could not compile program: ' + gl.getProgramInfoLog(program));
+  }
+
+  gl.useProgram(program);
+
+  return program;
+}
+
+
+function createBuffer3D(gl, vertices) {
+  var buffer = gl.createBuffer();
+  // buffer.itemSize = 3;
+  // buffer.numItems = vertices.length / 3;
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  return buffer;
+}
+
+function initializeGL(gl) {
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  // enable depth testing
+  gl.enable(gl.DEPTH_TEST);
+  // less-than-or-equal so that near things obscure far things
+  gl.depthFunc(gl.LEQUAL);
+
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // Set clear color to half-gray
+  gl.clearColor(0.5, 0.5, 0.5, 1.0);
+}
+
+// this is triggered after readyState == 'interactive', with the first canvas on the page.
+document.addEventListener('canvasready', function(ev) {
+  // log('canvasready', ev);
+  var canvas = ev.detail;
+  var gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+  if (gl) {
+    initializeGL(gl);
+    var gl_event = new CustomEvent('glready', {detail: gl});
+    document.dispatchEvent(gl_event);
+  }
+  else {
+    throw new Error('Your browser does not support WebGL.');
+  }
+});
+
+
+// MDN for `document` https://developer.mozilla.org/en-US/docs/Web/API/Document
+document.addEventListener('readystatechange', function(ev) {
+  // log('readystatechange', document.readyState);
+  if (document.readyState == 'interactive') {
+    var el = document.querySelector('canvas');
+    // detail is a magic word in the event constructor
+    var canvas_event = new CustomEvent('canvasready', {detail: el});
+    document.dispatchEvent(canvas_event);
+  }
+});
